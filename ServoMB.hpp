887e2cfee190bf92f72c380d5440f2e866aaa148
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2026-02-25 03:34:46 -0300

    FIX: Corrigindo tipagem e construtor

--- ServoMB.hpp
+++ ServoMB.hpp
@@ -7,21 +7,21 @@ Cabeçalho responsável pelo controle do servo motor.
 #include <Arduino.h>
 
 // Definição do valor da frequência do sinal PWM do servo motor
-#define FREQUENCIA_DO_SINAL_PWM_DO_SERVO_MOTOR 50
+constexpr uint32_t FREQUENCIA_DO_SINAL_PWM_DO_SERVO_MOTOR = 50;
 
 // Definição da resolução do sinal PWM
-#define RESOLUCAO_DO_SINAL_PWM_DO_SERVO_MOTOR 15
+constexpr uint8_t RESOLUCAO_DO_SINAL_PWM_DO_SERVO_MOTOR = 15;
 
 // Definição das porcentagens do ciclo de trabalho do sinal PWM
 // Período do sinal PWM: 20000us
 // Valor mínimo do ciclo de trabalho: 500us
 // Valor médio do ciclo de trabalho: 1450us
 // Valor máximo do ciclo de trabalho: 2400us
-#define PORCENTAGEM_MINIMA_DO_CICLO_DE_TRABALHO_DO_SERVO_MOTOR 0.025  // Servo motor na posição 0°
-#define PORCENTAGEM_MEDIA_DO_CICLO_DE_TRABALHO_DO_SERVO_MOTOR  0.0725 // Servo motor na posição 90°
-#define PORCENTAGEM_MAXIMA_DO_CICLO_DE_TRABALHO_DO_SERVO_MOTOR 0.12   // Servo motor na posição 180°
+constexpr float PORCENTAGEM_MINIMA_DO_CICLO_DE_TRABALHO_DO_SERVO_MOTOR = 0.025f;  // Servo motor na posição 0°
+constexpr float PORCENTAGEM_MEDIA_DO_CICLO_DE_TRABALHO_DO_SERVO_MOTOR  = 0.0725f; // Servo motor na posição 90°
+constexpr float PORCENTAGEM_MAXIMA_DO_CICLO_DE_TRABALHO_DO_SERVO_MOTOR = 0.12f;   // Servo motor na posição 180°
 
-constexpr unsigned short int VALOR_MAXIMO_RESOLUCAO_PWM_SERVO = (1 << RESOLUCAO_DO_SINAL_PWM_DO_SERVO_MOTOR) - 1;
+constexpr unsigned int VALOR_MAXIMO_RESOLUCAO_PWM_SERVO = (1 << RESOLUCAO_DO_SINAL_PWM_DO_SERVO_MOTOR) - 1;
 
 // Constantes com os valores do ciclo de trabalho do sinal PWM
 constexpr unsigned short int VALOR_MINIMO_DO_CICLO_DE_TRABALHO_DO_SERVO_MOTOR = (unsigned short int)(VALOR_MAXIMO_RESOLUCAO_PWM_SERVO * PORCENTAGEM_MINIMA_DO_CICLO_DE_TRABALHO_DO_SERVO_MOTOR);
@@ -34,10 +34,10 @@ class ServoMB {
 
     // Atributo que armazena o canal utilizado pelo pino de sinal do servo motor
     // Esse canal é responsável por gerar o sinal PWM
-    int _canal;
+    uint8_t _canal;
 
     // Atributo que armazena o valor da posição atual do servo motor
-    int _posicao = 0;
+    uint8_t _posicao = 0;
 
     public:
     
@@ -47,7 +47,7 @@ class ServoMB {
      * @param pino Número do pino de sinal do servo motor;
      * @param canal Canal de PWM do pino de sinal do servo motor.
      */
-    Servo(int pino, int canal) :
+    ServoMB(uint8_t pino, uint8_t canal) :
         // Atribui os valores passados nos atributos
         _canal(canal)
     {
@@ -63,7 +63,7 @@ class ServoMB {
      * 
      * @param posicao Valor da posição, na qual varia no intervalo [0, 180], para aplicar no servo motor.
      */
-    void setPosicao(int posicao) {
+    void setPosicao(uint8_t posicao) {
         /*
         Remapeia esse valor do intervalo:
         
@@ -80,7 +80,7 @@ class ServoMB {
         _posicao = constrain(posicao, 0, 180);
 
         // Remapeia o valor da posição do servo motor para o intervalo do ciclo de trabalho do sinal PWM
-        int posicaoRemapeada = map(
+        uint32_t posicaoRemapeada = (uint32_t)map(
             posicao,
             0,
             180,
@@ -97,7 +97,5 @@ class ServoMB {
      * 
      * @return O valor da posição atual do servo motor.
      */
-    int getPosicao() const {
-        return _posicao;
-    }
+    uint8_t getPosicao() const { return _posicao; }
 };
