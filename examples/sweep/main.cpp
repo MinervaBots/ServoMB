0d051f12dc744779f3f92738a15b5d582871664d
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2026-02-28 14:57:07 -0300

    ADD: Exemplo sweep do servo

---
+++ examples/sweep/main.cpp
@@ -0,0 +1,20 @@
+#include <ServoMB.h>
+
+constexpr uint8_t PINO_SERVO = 18;
+constexpr uint8_t CANAL_SERVO = 0;
+
+ServoMB servo(PINO_SERVO, CANAL_SERVO);
+
+void setup() {}
+
+void loop {
+    for (uint8_t posicao = 0; posicao <= 180; posicao++) {
+        servo.setPosicao(posicao);
+        delay(15);
+    }
+
+    for (uint8_t posicao = 180; posicao > 0; posicao--) {
+        servo.setPosicao(posicao);
+        delay(15);
+    }
+}
